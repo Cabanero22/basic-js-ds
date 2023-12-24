76333ab7bb223b24e5f9ac9544b9aca3eae48a98
Cabanero22 <dimancho1@gmail.com>
2023-12-24 17:16:15 +0500

    init: start task binarytree, complete part of task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,17 +8,39 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.rootElement = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootElement;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.rootElement = addInside(this.rootElement, data);
+
+    function addInside (node, data) {
+     
+      // Проверяем
+
+      if (node === null) { // если узел отсутствует (null)
+        return new Node (data);
+      }
+
+      if(node.data === data) {  // если узел есть
+        return node
+      }
+
+      if (node.data > data) { // если текущее значении больше добавляемого
+        node.left = addInside (node.left, data);
+      } else {
+        node.right = addInside (node.right, data);
+      }
+      return node; // возвращаем текущий узел;
+    }
   }
 
-  has(/* data */) {
+  has(data) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
